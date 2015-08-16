0ec5aba472aac06fdc3e7eaa307b934e85f1d5a2
Juraj Kucera <kucera.duri@gmail.com>
2015-08-16 18:32:42 +0200

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * Created by duri on 8/16/2015.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("bcsg technical tast started, Created By Juraj Kucera");
+    }
+
+}
---
+++ src/test/java/BankcardServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by duri on 8/16/2015.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class BankcardServiceTest {
+
+
+    @Test
+    public void nothing() throws Exception {
+
+    }
+
+}
